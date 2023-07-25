1f0c09beb68c0517b5492d43e948bec3e7b88c67
Gareth Grindeland <gt.grindeland@gmail.com>
2023-07-25 10:00:10 -0700

    define types for Product, Image, Size, and Color

--- ecommerce-store/types.ts
+++ ecommerce-store/types.ts
@@ -3,7 +3,7 @@
 // declare a JS object conforming to these types like this...
 // const billboard: Billboard = {
 //   id: "Company"
-//   name: "Advertisement"
+//   label: "Advertisement"
 //   imageUrl: "https//..."
 // }
 
@@ -18,3 +18,31 @@ export interface Category {
   name: string;
   billboard: Billboard;
 };
+
+export interface Product {
+  id: string;
+  category: Category;
+  name: string;
+  price: string;
+  isFeatured: boolean;
+  size: Size;
+  color: Color;
+  images: Image[];
+}
+
+export interface Image {
+  id: string;
+  url: string;
+}
+
+export interface Size {
+  id: string;
+  name: string;
+  value: string;
+}
+
+export interface Color {
+  id: string;
+  name: string;
+  value: string;
+}
