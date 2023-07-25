0e94d2c2c5da15d6cc03b285985a2e8f09d5c936
Gareth Grindeland <gt.grindeland@gmail.com>
2023-07-25 11:05:14 -0700

    add No-results component that renders a message if the product list is empty

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,10 +1,13 @@
 import getBillboard from "@/actions/get-billboard";
 import Container from "@/components/ui/Container";
 import Billboard from "@/components/Billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/Product-list";
 
 export const revalidate = 0; //clear cached data
 
 const HomePage = async () => {
+  const products = await getProducts({isFeatured: true});
   const billboard = await getBillboard(`871631ff-b05c-47c8-892f-074dc3e5af69`);
 
   return (
@@ -12,6 +15,9 @@ const HomePage = async () => {
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
       </div>
     </Container>
   );
--- ecommerce-store/components/Product-list.tsx
+++ ecommerce-store/components/Product-list.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@/types";
+import NoResults from "./ui/No-results";
 
 interface ProductListProps {
   title: string;
@@ -11,7 +12,8 @@ const ProductList: React.FC<ProductListProps> = ({
 }) => {
   return (
     <div className="space-y-4">
-      <h3>{title}</h3>
+      <h3 className="font-bold text-3xl">{title}</h3>
+      {items.length === 0 && <NoResults />}
     </div>
   )
 }
