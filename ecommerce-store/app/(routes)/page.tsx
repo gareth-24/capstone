c06b6433d16ee1ea7d33113eb2117acf33a61fea
Gareth Grindeland <gt.grindeland@gmail.com>
2023-07-25 09:49:09 -0700

    refactor page.tsx to import billboard data using a test billboard

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,6 +1,19 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Container from "@/components/ui/Container";
+import Billboard from "@/components/Billboard";
+
+export const revalidate = 0; //clear cached data
+
+const HomePage = async () => {
+  const billboard = await getBillboard(`871631ff-b05c-47c8-892f-074dc3e5af69`);
+
   return (
-    <div>This is the store</div>
+    //This is the store
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
   );
 }
 
