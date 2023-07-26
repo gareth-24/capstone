56f9fdf9fdb519b91176e43dac7a4e8231034a69
Gareth Grindeland <gt.grindeland@gmail.com>
2023-07-26 14:47:02 -0700

    update preview-model component

--- ecommerce-store/components/Preview-model.tsx
+++ ecommerce-store/components/Preview-model.tsx
@@ -1,15 +1,33 @@
 "use client";
 
 import usePreviewModel from "@/hooks/use-preview-model";
+import Model from "@/components/ui/Model";
+import Gallery from "@/components/gallery";
+import Info from "@/components/Info";
 
 const PreviewModel = () => {
   const previewModel = usePreviewModel();
   const product = usePreviewModel((state) => state.data);
 
-  return (
-    <div>
+  if (!product) {
+    return null;
+  }
 
-    </div>
+  return (
+    <Model
+      open={previewModel.isOpen}
+      onClose={previewModel.onClose}
+    >
+      <div className="grid w-full grid-cols-1 items-start gap-x-6 pag-y-8 sm:grid-cols-12 lg:gap-x-8">
+        <div className="sm:col-span-4 lg:col-span-5">
+          <Gallery images={product.images}/>
+        </div>
+        <div className="sm:col-span-8 lg:col-span-7">
+          <Info data={product} />
+        </div>
+      </div>
+
+    </Model>
   );
 }
 
