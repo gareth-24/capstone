4ae50306a7855f082bddaffd886bb16aa56c442e
Gareth Grindeland <gt.grindeland@gmail.com>
2023-07-26 10:56:05 -0700

    add styling and popover dialogue pannel for the mobile filter onclick

--- ecommerce-store/app/(routes)/category/[categoryId]/components/Mobile-filters.tsx
+++ ecommerce-store/app/(routes)/category/[categoryId]/components/Mobile-filters.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { useState } from "react";
+import { Plus, X } from "lucide-react";
+import { Dialog } from "@headlessui/react";
 
 import { Color, Size } from "@/types";
 import Button from "@/components/ui/Button";
-import { Plus } from "lucide-react";
+import IconButton from "@/components/ui/Icon-button";
 
 interface MobileFiltersProps {
   sizes: Size[];
@@ -26,6 +28,23 @@ const MobileFilters: React.FC<MobileFiltersProps>= ({
         Filters
         <Plus size={20} />
       </Button>
+
+      <Dialog open={open} className="flex items-center gap-x-2 lg:hidden" onClose={onClose}>
+        {/* Background */}
+        <div className="fixed inset-0 bg-black bg-opacity-25" />
+
+        {/* Dialog Position */}
+        <div className="fixed inset-0 z-40 flex">
+          <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
+
+            {/* Close Button */}
+            <div className="flex items-center justify-end px-4">
+              <IconButton icon={<X size={15} />} onClick={onClose} />
+            </div>
+          </Dialog.Panel>
+
+        </div>
+      </Dialog>
     </>
   );
 }
