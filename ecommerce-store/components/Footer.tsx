f839d70c17de54f389125324de3126aa6432d4ea
Gareth Grindeland <gt.grindeland@gmail.com>
2023-07-26 11:35:37 -0700

    update navbar and footer

--- ecommerce-store/components/Footer.tsx
+++ ecommerce-store/components/Footer.tsx
@@ -3,7 +3,8 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
-          &copy; 2024 StoreName, Inc. All rights reserved.
+          {/* &copy; 2023 Xlerate, Inc. All rights reserved. */}
+          This is a demo e-commerce store. Products are not for sale.
         </p>
       </div>
 
