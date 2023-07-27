620107d088a4c0e379f8e8718e14397079d93dba
Gareth Grindeland <gt.grindeland@gmail.com>
2023-07-27 11:12:24 -0700

    add navbar styling

--- ecommerce-store/components/Navbar-actions.tsx
+++ ecommerce-store/components/Navbar-actions.tsx
@@ -17,7 +17,7 @@ const NavbarActions = () => {
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+      <Button className="flex items-center rounded-full bg-green-700 px-5 py-2">
         <ShoppingBag
           size={20}
           color="white"
