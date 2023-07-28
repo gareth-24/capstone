ed31a7c1d1596143e8de7d00fdc7200af721852d
Gareth Grindeland <gt.grindeland@gmail.com>
2023-07-28 11:17:20 -0700

    add toast-provider

--- ecommerce-store/app/layout.tsx
+++ ecommerce-store/app/layout.tsx
@@ -4,6 +4,7 @@ import { Urbanist } from 'next/font/google'
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import ModelProvider from '@/providers/model-provider'
+import ToastProvider from '@/providers/toast-provider'
 
 const font = Urbanist({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ModelProvider />
+        <ToastProvider />
         <Navbar />
         {children}
         <Footer />
